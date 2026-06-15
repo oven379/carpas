71384a102b7263fabc87c95a44883eeb1cd275da
KulikovAR <159744787+KulikovAR@users.noreply.github.com>
2026-06-15 16:08:21 +0300

    Проба

--- src/ui/DevHud.jsx
+++ src/ui/DevHud.jsx
@@ -1,13 +1,3 @@
-import { useEffect } from 'react'
-import { isNativeApp } from '../lib/nativePlatform.js'
-
 export default function DevHud() {
-  useEffect(() => {
-    if (!import.meta.env.DEV && !isNativeApp()) return
-    import('eruda').then(({ default: eruda }) => {
-      eruda.init()
-    })
-  }, [])
-
   return null
 }
